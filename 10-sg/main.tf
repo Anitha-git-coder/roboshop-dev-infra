70142d4395891b679cc787becb921ffe41b9f91a
Anitha <anitha.pinipe14@gmail.com>
2026-01-19 10:18:19 +0530

    terraform

---
+++ 10-sg/main.tf
@@ -0,0 +1,10 @@
+module "catalogue" {
+  source = "terraform-aws-modules/security-group/aws"
+ 
+  name = "${local.common_name_suffix} -catalogue" 
+  use_name_prefix   = false 
+  description = "security group for catalogue with custom ports open within vpc, egress all traffic"
+  vpc_id = data.aws_ssm_parameter.vpc_id.value
+}
+
+
