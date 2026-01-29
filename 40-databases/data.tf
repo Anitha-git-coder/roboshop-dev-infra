f4d8d2f1f3bc7392ea55d54541b73122e92cc843
Anitha <anitha.pinipe14@gmail.com>
2026-01-29 20:34:21 +0530

    terraform

--- 40-databases/data.tf
+++ 40-databases/data.tf
@@ -24,8 +24,8 @@ data "aws_ami" "joindevops" {
   name = "/${var.project_name}/${var.environment}/bastion_sg_id"
 }
 
-data "aws_ssm_parameter" "public_subnet_ids" {
-  name = "/${var.project_name}/${var.environment}/public_subnet_ids"
+data "aws_ssm_parameter" "database_subnet_ids" {
+  name = "/${var.project_name}/${var.environment}/database_subnet_ids"
 } 
 
 
