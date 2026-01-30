6fbe70c00237cc70b33065536a6e551d7f3e8d35
Anitha <anitha.pinipe14@gmail.com>
2026-01-30 14:22:28 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,4 +1,9 @@
+# #!/bin/bash
+
+# dnf install ansible -y
+# ansible pull -U https://github.com/Anitha-git-coder/ansible-roboshop-roles-terraform.git -e component = mongodb main.yaml
+
 #!/bin/bash
 
 dnf install ansible -y
-ansible pull -U https://github.com/Anitha-git-coder/ansible-roboshop-roles-terraform.git -e component = mongodb main.yaml
+ansible-pull -U https://github.com/Anitha-git-coder/ansible-roboshop-roles-terraform.git main.yaml -e component=mongodb
