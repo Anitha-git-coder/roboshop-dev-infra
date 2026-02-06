676775c276c72135d8dcaad6f88cc7b61f7e64fb
Anitha <anitha.pinipe14@gmail.com>
2026-02-06 14:09:34 +0530

    terraform

--- 50-backend-alb/variables.tf
+++ 50-backend-alb/variables.tf
@@ -5,3 +5,11 @@ variable "project_name" {
 variable "environment" {
     default = "dev"  
 }
+
+variable "domain_name" {
+  default = "anitha.fun"
+}
+
+variable "zone_id" {
+  default = "Z08957613Q2VP6KYK490C"
+}
