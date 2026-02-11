b79c322161d3091f255fe233d67bee81221239d3
Anitha <anitha.pinipe14@gmail.com>
2026-02-11 13:24:07 +0530

    terraform

--- 20-bastion/bastion.sh
+++ 20-bastion/bastion.sh
@@ -5,28 +5,60 @@ growpart /dev/nvme0n1 4
 lvextend -L +30G /dev/mapper/RootVG-homeVol
 xfs_growfs /home
 
-sudo yum install -y yum-utils 
-sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
-sudo yum install terraform
+sudo yum install -y yum-utils
+sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/RHEL/hashicorp.repo
+sudo yum -y install terraform
 
-REPO_URL=https://github.com/Anitha-git-coder/ansible-roboshop-roles-tf.git
-REPO_DIR=/opt/roboshop/ansible/
-ANSIBLE_DIR=ansible-roboshop-roles-tf
+# sudo lvreduce -r -L 6G /dev/mapper/RootVG-rootVol
 
-mkdir -p $REPO_DIR
-mkdir -p /var/log/roboshop/
-touch ansible.log
+# creating databases
+cd /home/ec2-user
+git clone https://github.com/Anitha-git-coder/roboshop-dev-infra.git
+chown ec2-user:ec2-user -R roboshop-dev-infra
+cd roboshop-dev-infra/40-databases
+terraform init
+terraform apply -auto-approve
 
-cd $REPO_DIR
-# check if ansible already clone or not
 
-if [ -d $ANSIBLE_DIR ]; then
+
+
+
+
+
+
+
+
+
+
+# #!/bin/bash
+
+# # growing the /home volume for terraform purpose
+# growpart /dev/nvme0n1 4
+# lvextend -L +30G /dev/mapper/RootVG-homeVol
+# xfs_growfs /home
+
+# sudo yum install -y yum-utils 
+# sudo yum-config-manager --add-repo https://rpm.releases.hashicorp.com/AmazonLinux/hashicorp.repo
+# sudo yum install terraform
+
+# REPO_URL=https://github.com/Anitha-git-coder/ansible-roboshop-roles-tf.git
+# REPO_DIR=/opt/roboshop/ansible/
+# ANSIBLE_DIR=ansible-roboshop-roles-tf
+
+# mkdir -p $REPO_DIR
+# mkdir -p /var/log/roboshop/
+# touch ansible.log
+
+# cd $REPO_DIR
+# # check if ansible already clone or not
+
+# if [ -d $ANSIBLE_DIR ]; then
  
- cd $ANSIBLE_DIR #open the dir
- git pull  
- else
- git clone $REPO_URL 
- cd $ANSIBLE_DIR 
- fi
+#  cd $ANSIBLE_DIR #open the dir
+#  git pull  
+#  else
+#  git clone $REPO_URL 
+#  cd $ANSIBLE_DIR 
+#  fi
 
- ansible-playbook -e component=$component main.yaml
+#  ansible-playbook -e component=$component main.yaml
---
+++ 90-components/variables.tf
@@ -0,0 +1,16 @@
+variable "project_name" {
+    default = "roboshop"  
+}
+
+variable "environment" {
+    default = "dev"  
+}
+
+variable "component" {
+  default = "catalogue" 
+}
+
+variable "rule_priority" {
+  default = "catalogue" 
+
+}
