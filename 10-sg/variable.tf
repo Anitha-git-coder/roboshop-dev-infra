a80148565ed353edcec17d6ccd7096277732c9c7
Anitha <anitha.pinipe14@gmail.com>
2026-02-18 12:59:58 +0530

    terraform

--- 10-sg/variable.tf
+++ 10-sg/variable.tf
@@ -18,7 +18,8 @@ variable "sg_names" {
     # frontend load balancer
     "frontend_alb",
     #backend alb
-    "backend_alb"
+    "backend_alb",
+    "open_vpn"
 
     ]
 }
---
+++ 21-vpn/locals.tf
@@ -0,0 +1,11 @@
+locals {
+  ami_id = data.aws_ami.openvpn.id #"ami-09c813fb71547fc4f" 
+  open_vpn_sg_id = data.aws_ssm_parameter.open_vpn_sg_id.value
+  public_subnet_id = split("," , data.aws_ssm_parameter.public_subnet_ids.value)[0]
+ 
+  common_tags = {
+    Project = var.project_name
+    Environment = var.environment
+    terraform = "true"
+  }
+}
